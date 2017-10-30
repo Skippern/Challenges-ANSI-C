11314ab3e1b56143c82ad86a8b59eb22cdf27bcf
Aun Johnsen <Skippern@users.noreply.github.com>
2017-10-30 11:03:23 +0300

    Char

---
+++ char/char.c
@@ -0,0 +1,19 @@
+/*
+ * Challenge taken from Facebook
+ *
+ * What will be printed
+ *
+ * [A] 128
+ * [B] -128
+ * [C] Depends on the compiler
+ * [D] None of the mentioned
+ */
+#include <stdio.h>
+
+int main()
+{
+    char chr;
+    chr = 128;
+    printf("%d\n", chr);
+    return 0;
+}
