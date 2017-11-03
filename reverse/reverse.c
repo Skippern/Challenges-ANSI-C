43b522e1c898f10bec62db20f4f15663e706d08e
Aun Johnsen <Skippern@users.noreply.github.com>
2017-11-03 15:25:50 +0300

    reverse

---
+++ reverse/reverse.c
@@ -0,0 +1,23 @@
+/*
+ * Challenge from Facebook
+ *
+ * Comment your answer
+ *
+ * How to get the reverse output
+ */
+#include <stdio.h>
+int main()
+{
+    int *p, i;
+    static int x[5] = {5, 9, 6, 3, 7};
+    i = 0;
+    p = x;
+    printf("Element value Addresses\n\n");
+    while(i<5)
+    {
+        printf("x[%d] %d \n", i, *p);
+        i++;
+        p++;
+    }
+    return 0;
+}
