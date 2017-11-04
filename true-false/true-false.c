44ac8abca7506008b5bd0c3059efe55a5df94f45
Aun Johnsen <Skippern@users.noreply.github.com>
2017-11-04 09:21:10 +0300

    true/false

---
+++ true-false/true-false.c
@@ -0,0 +1,17 @@
+/*
+ * Challenge from facebook
+ *
+ * What will be the output of the program?
+ *
+ * [A] k == 1 is TRUE
+ * [B] 1 == 1 is TRUE
+ * [C] 1 == 1 is FALSE
+ * [D] k == 1 is FALSE
+ */
+#include <stdio.h>
+int main()
+{
+    int k=1;
+    printf("%d == 1 is" "%s\n", k, k==1?"TRUE":"FALSE");
+    return 0;
+}
