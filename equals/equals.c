72ec58ef21a5e8e880c824470658d5f232ade666
Aun Johnsen <Skippern@users.noreply.github.com>
2017-11-10 17:05:03 +0300

    Equals

---
+++ equals/equals.c
@@ -0,0 +1,12 @@
+/*
+ * Challenge from Facebook
+ */
+#include <stdio.h>
+
+int main()
+{
+    int n = 2, m = 3;
+    if (n = m) printf("Equals\n");
+    else printf("Not Equal\n");
+    return 0; 
+}
