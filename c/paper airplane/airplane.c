0f952816123f36754b03faac8dfd71b9656b52e1
Aun Johnsen <Skippern@users.noreply.github.com>
2017-12-11 20:52:25 +0300

    Paper Airplanes

---
+++ c/paper airplane/airplane.c
@@ -0,0 +1,18 @@
+/*
+ * Joke from facebook
+ *
+ * Teacher give him punishment
+ * to write 5000 times, 
+ * "I will not throw paper airplanes in class"
+ * & submit it tomorrow
+ *
+ * Next day, he submitted the paper written,
+ */
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int n;
+    for(n=1;n<=5000;n++) printf("I will not throw paper airplanes in class");
+    getch();
+}
