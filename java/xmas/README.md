f9fe58430337aa98e4f139896884b5557781f1f1
Aun Johnsen <Skippern@users.noreply.github.com>
2017-12-25 09:19:00 +0300

    Holiday greeting

---
+++ java/xmas/README.md
@@ -0,0 +1,3 @@
+# `XmasHolidays.java`
+
+Taken from facebook. It is meant as a greeting, and will not compile. It does not follow any java code standard, and bears resemblences from obfuscated code (though a true obfuscated code will compile).
---
+++ java/xmas/XmasHolidays.java
@@ -0,0 +1,18 @@
+               //\\
+              ///\\\
+            /////\\\\\
+          import java.io;
+         import javac.xmas;
+        class XmasHolidays {
+       String countDown(dd) {
+      return((25)-dd)*24+'h';
+     }void toSanta(String gift){
+    send("Dear Santa\n\t"+gift);
+  }public boolean doesSantaExist(){
+  while(true) return true; continue;
+} void IwantXmasBack(){return false}}
+                |||||
+                |||||
+
+    MERRY CHRISTMAS FROM JAVA & VIEMME!
+    While(isXmas) stayHappy();
