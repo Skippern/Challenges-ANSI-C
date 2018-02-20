a971bbb02f6fd11dc7d11f658400074234e6013d
Aun Johnsen <lists@gimnechiske.org>
2018-02-20 18:56:05 +0100

    Compare

---
+++ c/compare/compare.c
@@ -0,0 +1,21 @@
+/*
+ * Challenge found on facebook
+ * 
+ * OUTPUT?
+ * A> YES
+ * B> NO
+ * C> Error
+ */
+#include <stdio.h>
+
+void main()
+{
+    double x = 3.1;
+    float y = 3.1;
+
+    if (x==y)
+        printf("YES");
+    else
+        printf("NO");
+
+}
